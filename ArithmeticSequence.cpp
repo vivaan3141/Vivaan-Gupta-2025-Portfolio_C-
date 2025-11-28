1efeba38faf87c48c42ba17f8cb7c2783d22feb3
vivaan3141 <155184535+vivaan3141@users.noreply.github.com>
2025-11-27 18:03:29 -0800

    Implement arithmetic sequence calculation
    This program calculates the equation of an arithmetic sequence based on user input and finds a specific term in the series.

---
+++ ArithmeticSequence.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){   
+    
+    int c[]={};
+    int a;
+    
+    for (int i=0; i<3; i++){
+        cout<<"Number"<<i+1<<":";
+        cin>>a;
+        c[i]=a;
+    }
+    int n;
+    int two= c[1];
+    int three=c[2];
+    int d=three-two;
+    int com=c[0]-d;
+    
+    if (com>0){
+        cout<<"The equation of this series is "<<d<<"n"<<"+"<<com<<endl;  
+    } else if (com<0){
+        cout<<"The equation of this series is "<<d<<"n"<<com<<endl;  
+    }else if (com==0){
+        cout<<"The equation of this series is "<<d<<"n"<<endl;  
+    }
+    int val=0;
+    int find=0;
+    cout<<"What number in the series would you like to find?"<<endl;
+    cin>>val;
+    find=(d*val);
+    cout<<"The "<<val<<" number in this series is "<<find+com;
+      
+    return 0;
+}
+
+
